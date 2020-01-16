d4d2d147a8f04265d81965ee9ec5bdbc6598d700
Harsh Verma <verma.harsh98@gmail.com>
2020-01-16 22:18:25 +0530

    Updates

---
+++ precache-manifest.e24bc46e001b2c6380584e31e14b4f5d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10fca8188100b20d4d520bcc5cd811f6",
+    "url": "/React-app-basic/index.html"
+  },
+  {
+    "revision": "0e67228b79c6df44f9f6",
+    "url": "/React-app-basic/static/css/2.275f7c89.chunk.css"
+  },
+  {
+    "revision": "ccab10e1c2799ece1390",
+    "url": "/React-app-basic/static/css/main.6a1f39fb.chunk.css"
+  },
+  {
+    "revision": "0e67228b79c6df44f9f6",
+    "url": "/React-app-basic/static/js/2.3f78cbd0.chunk.js"
+  },
+  {
+    "revision": "ccab10e1c2799ece1390",
+    "url": "/React-app-basic/static/js/main.9371ae11.chunk.js"
+  },
+  {
+    "revision": "29472f42b78d6fc3cf13",
+    "url": "/React-app-basic/static/js/runtime~main.d27990a7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-app-basic/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/React-app-basic/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-app-basic/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-app-basic/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-app-basic/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
